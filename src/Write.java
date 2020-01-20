ec853bb0e3f4ecc35697458aaad3a386b32b2f91
zkrzyzan <01149367@pw.edu.pl>
2020-01-20 14:58:28 +0100

    matrix equals

--- src/Write.java
+++ src/Write.java
@@ -1,5 +1,9 @@
 public class Write {
 
+    /**
+     * Wypisanie przykładowej macierzy
+     * @param args
+     */
     public static void main (String[] args){
         double [][] a = {{1, 2}, {3, 4}};
         Matrix A = new Matrix(a);

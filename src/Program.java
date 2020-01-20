b39c1ee19c6efc6cc3f417d163ca3f0c59bf8d27
RafalRoter <rafalroter00@gmail.com>
2020-01-20 19:49:26 +0100

    Program wykorzystujący działania na macierzach - wersja pełna

--- src/Program.java
+++ src/Program.java
@@ -2,46 +2,215 @@ import java.util.Scanner;
 
 public class Program {
 
-    public static void main(String[] args){
-        Scanner scan=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Wybierz działanie jakie chciałbyś wykonać:");
         System.out.println("1-Dodawanie macierzy");
         System.out.println("2-Odejmowanie macierzy");
         System.out.println("3-Transpozycja macierzy");
-        System.out.println("4-Mnożenie macierzy przez skalar");
-        System.out.println("5-Obliczenie wyznacznika macierzy");
-        System.out.println("6-Sprawdzenie czy macierze są równe");
-        System.out.println("7-Wypisanie macierzy");
-        System.out.println("0-Wyłączenie programu");
-       int a=scan.nextInt();
-        if(a==1) {
+        System.out.println("4-Mnożenie macierzy");
+        System.out.println("5-Mnożenie macierzy przez skalar");
+        System.out.println("6-Obliczenie wyznacznika macierzy");
+        System.out.println("7-Sprawdzenie czy macierze są równe");
+        System.out.println("8-Wypisanie macierzy");
+        System.out.println("Inna liczba-Wyłączenie programu");
+        int a = scan.nextInt();
+        if (a == 1) {
             System.out.println("Podaj wielkość macierzy:");
 
-            Scanner scan2=new Scanner(System.in);
-            int b=scan2.nextInt();
-            int c=scan2.nextInt();
-            Matrix pierwsza=new Matrix(b,c);
-            Matrix druga=new Matrix(b,c);
-            double[][] pierwszaM=new double[b][c];
-            double[][] drugaM=new double[b][c];
-            for(int i=0; i<b;i++)for(int j=0;j<c;j++) {
-                System.out.println("Wprowadź wartość macierzy pierwszej na pozycji ["+i+"]["+j+"]:");
-                pierwszaM[i][j] = scan2.nextDouble();
-            }
-            for(int i=0; i<b;i++)for(int j=0;j<c;j++){
-                System.out.println("Wprowadź wartość macierzy drugiej na pozycji ["+i+"]["+j+"]:");
-                drugaM[i][j]=scan2.nextDouble();
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Liczba kolumn:");
+            int b = scan2.nextInt();
+            System.out.println("Liczba wierszy:");
+            int c = scan2.nextInt();
+            double[][] pierwszaM = new double[b][c];
+            double[][] drugaM = new double[b][c];
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy pierwszej na pozycji [" + i + "][" + j + "]:");
+                    pierwszaM[i][j] = scan2.nextDouble();
+                }
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy drugiej na pozycji [" + i + "][" + j + "]:");
+                    drugaM[i][j] = scan2.nextDouble();
 
-            }
-            Matrix pierwszaMa=new Matrix(pierwszaM);
-            Matrix drugaMa=new Matrix(drugaM);
-            Matrix suma=pierwszaMa.addition(drugaMa);
+                }
+            Matrix pierwszaMa = new Matrix(pierwszaM);
+            Matrix drugaMa = new Matrix(drugaM);
+            Matrix suma = pierwszaMa.addition(drugaMa);
             suma.show();
+
+        }
+        if (a == 2) {
+            System.out.println("Podaj wielkość macierzy:");
+
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Liczba kolumn:");
+            int b = scan2.nextInt();
+            System.out.println("Liczba wierszy:");
+            int c = scan2.nextInt();
+            double[][] pierwszaM = new double[b][c];
+            double[][] drugaM = new double[b][c];
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy pierwszej na pozycji [" + i + "][" + j + "]:");
+                    pierwszaM[i][j] = scan2.nextDouble();
+                }
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy drugiej na pozycji [" + i + "][" + j + "]:");
+                    drugaM[i][j] = scan2.nextDouble();
+
+                }
+            Matrix pierwszaMa = new Matrix(pierwszaM);
+            Matrix drugaMa = new Matrix(drugaM);
+            Matrix roznica = pierwszaMa.subtraction(drugaMa);
+            roznica.show();
+
+        }
+
+
+        if (a == 3) {
+            System.out.println("Podaj wielkość macierzy:");
+
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Liczba kolumn:");
+            int b = scan2.nextInt();
+            System.out.println("Liczba wierszy:");
+            int c = scan2.nextInt();
+            double[][] pierwszaM = new double[b][c];
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy pierwszej na pozycji [" + i + "][" + j + "]:");
+                    pierwszaM[i][j] = scan2.nextDouble();
+                }
+            Matrix pierwszaMa = new Matrix(pierwszaM);
+
+            Matrix transpozycja = pierwszaMa.transposition();
+            transpozycja.show();
+
+        }
+        if (a == 4) {
+            System.out.println("Podaj wielkość macierzy:");
+
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Liczba kolumn:");
+            int b = scan2.nextInt();
+            System.out.println("Liczba wierszy:");
+            int c = scan2.nextInt();
+            double[][] pierwszaM = new double[b][c];
+            double[][] drugaM = new double[b][c];
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy pierwszej na pozycji [" + i + "][" + j + "]:");
+                    pierwszaM[i][j] = scan2.nextDouble();
+                }
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy drugiej na pozycji [" + i + "][" + j + "]:");
+                    drugaM[i][j] = scan2.nextDouble();
+
+                }
+            Matrix pierwszaMa = new Matrix(pierwszaM);
+            Matrix drugaMa = new Matrix(drugaM);
+            Matrix mnozenie = pierwszaMa.multiplication(drugaMa);
+            mnozenie.show();
+
+        }
+
+
+        if (a == 5) {
+            System.out.println("Podaj wielkość macierzy:");
+
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Liczba kolumn:");
+            int b = scan2.nextInt();
+            System.out.println("Liczba wierszy:");
+            int c = scan2.nextInt();
+            double[][] pierwszaM = new double[b][c];
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy pierwszej na pozycji [" + i + "][" + j + "]:");
+                    pierwszaM[i][j] = scan2.nextDouble();
+                }
+            System.out.println("Wprowadź wartość skalara:");
+            double scal = scan2.nextInt();
+
+
+            Matrix pierwszaMa = new Matrix(pierwszaM);
+            Matrix mnozeniescalar = pierwszaMa.scalar(scal);
+            mnozeniescalar.show();
+        }
+        if (a == 6) {
+            System.out.println("Podaj wielkość macierzy:");
+
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Liczba kolumn:");
+            int b = scan2.nextInt();
+            System.out.println("Liczba wierszy:");
+            int c = scan2.nextInt();
+            double[][] pierwszaM = new double[b][c];
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy pierwszej na pozycji [" + i + "][" + j + "]:");
+                    pierwszaM[i][j] = scan2.nextDouble();
+                }
+            Matrix pierwszaMa = new Matrix(pierwszaM);
+
+            Matrix wyznacznik = pierwszaMa.determinant();
+            wyznacznik.show();
+
+
+        }
+        if (a == 7) {
+            System.out.println("Podaj wielkość macierzy:");
+
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Liczba kolumn:");
+            int b = scan2.nextInt();
+            System.out.println("Liczba wierszy:");
+            int c = scan2.nextInt();
+            double[][] pierwszaM = new double[b][c];
+            double[][] drugaM = new double[b][c];
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy pierwszej na pozycji [" + i + "][" + j + "]:");
+                    pierwszaM[i][j] = scan2.nextDouble();
+                }
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy drugiej na pozycji [" + i + "][" + j + "]:");
+                    drugaM[i][j] = scan2.nextDouble();
+
+                }
+            Matrix pierwszaMa = new Matrix(pierwszaM);
+            Matrix drugaMa = new Matrix(drugaM);
+            boolean rowna= pierwszaMa.eq(drugaMa);
+            if(rowna)System.out.println("Macierze są równe");
+            else System.out.println("Macierze nie są równe");
+
+        }
+        if (a == 8) {
+            System.out.println("Podaj wielkość macierzy:");
+
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Liczba kolumn:");
+            int b = scan2.nextInt();
+            System.out.println("Liczba wierszy:");
+            int c = scan2.nextInt();
+            double[][] pierwszaM = new double[b][c];
+            for (int i = 0; i < b; i++)
+                for (int j = 0; j < c; j++) {
+                    System.out.println("Wprowadź wartość macierzy pierwszej na pozycji [" + i + "][" + j + "]:");
+                    pierwszaM[i][j] = scan2.nextDouble();
+                }
+
+            Matrix pierwszaMa = new Matrix(pierwszaM);
+
+            pierwszaMa.show();
+
         }
 
     }
-
-
-
-
 }
